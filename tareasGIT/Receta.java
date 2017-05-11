c39902ba36d8e238f6030c4715661fdbdf8ae96f
Manuel Quinto <maquimed@gmail.com>
2017-05-11 13:45:38 +0200

    Creación de constructores
    Signed-off-by: Manuel Quinto <maquimed@gmail.com>

--- tareasGIT/Receta.java
+++ tareasGIT/Receta.java
@@ -4,4 +4,14 @@ public class Receta {
 	String nombre;
 	String ingredientes;
 	String preparacion;
+	
+	public Receta() {
+		
+	}
+	public Receta(String nombre, String ingredientes, String preparacion) {
+		this.nombre = nombre;
+		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
+	
 }
