b6fd77cb7696d811a34aa6c657053427b3599909
Manuel Quinto <maquimed@gmail.com>
2017-05-18 12:55:46 +0200

    addPreparacion a Receta.java

--- tareasGIT/Receta.java
+++ tareasGIT/Receta.java
@@ -39,4 +39,7 @@ public class Receta {
 	public void addIngrediente(Ingrediente ingrediente) {
 		this.ingredientes.add(ingrediente);
 	}
+	public void addPreparacion(String preparacion) {
+		this.preparacion.add(preparacion);
+	}
 }
